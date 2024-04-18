410a4f25e7d6cb4de9192b8b12773506a5c411d2
seangleng <seangleng507@gmail.com>
2024-04-18 08:53:01 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Galcorp-logistic",
+  description: "Gal is the ideal international freight forwarder to suit your freight and logistics needs. ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html >
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ThreeDCardDemo } from "@/components/3dEffectCard";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ThreeDCardDemo />
+    </main>
+  );
+}
